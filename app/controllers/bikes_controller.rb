349499bb7d195f4ad38fd184330f51b56b1008eb
jason-nordheim <jason.nordheim@gmail.com>
2020-04-23 11:48:39 -0400

    removed frame size

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,4 +1,5 @@
 class BikesController < ApplicationController
+  before_action :find_coffee_by_id, only: [:show, :update, :destroy]
 
   def index 
     @bikes = Bike.all 
@@ -11,8 +12,31 @@ class BikesController < ApplicationController
 
   def create 
     @bike = Bike.new(
-      
+      brand: params[:brand], 
+      model: params[:model], 
+      description: params[:description], 
+      price: paramas[:price]
     )
+    if @bike.save
+      render json: @bike 
+    else 
+      render status: 422 
+    end 
   end 
+
+  def update
+    @bike = Bike.update(
+      brand: params[:brand], 
+      model: params[:model], 
+      description: params[:description], 
+      price: paramas[:price] 
+    )
+    render json: @bike 
+  end 
+
+  def destroy 
+    @coffee.destroy 
+    render json: {message: "You successfully destroyed the requested coffee."}
+  end
 
 end
